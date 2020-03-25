827dabca7a9eebf832ebec01a358da724483faba
Adil <adil@ajc-formation.com>
2020-03-25 18:02:51 +0100

    Initialisation Projet Spring Boot

--- RestaurantAmbulant/src/main/java/poeiklee/RestaurantAmbulant/RestaurantAmbulantApplication.java
+++ RestaurantAmbulant/src/main/java/poeiklee/RestaurantAmbulant/RestaurantAmbulantApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package poeiklee.RestaurantAmbulant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
