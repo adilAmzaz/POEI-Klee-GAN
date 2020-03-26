6fd9b69e5f848e4fe00ad89c85a32e791524bf5e
Gabriel MAYOUD <gabriel.mayoud@gmail.com>
2020-03-26 10:08:10 +0100

    front service port update

--- RestaurantAmbulantFront/src/app/models/global-config.ts
+++ RestaurantAmbulantFront/src/app/models/global-config.ts
@@ -1,4 +1,4 @@
 export class GlobalConfig {
-    public static serverUrl : string = "http://localhost:8081/"
+    public static serverUrl : string = "http://localhost:8083/"
     public static userEndPoint : string = `${GlobalConfig.serverUrl}user/`
 }
